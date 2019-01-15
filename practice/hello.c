25448268a52c8222d779d8fc8d914b0bd65d2dc1
yuanjieyue <yuanjieyue89@gmail.com>
2019-01-15 10:02:18 -0800

    Add a practice directory

---
+++ practice/hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+  //puts("Hello world\n");
+  int int1, int2;
+  printf("please enter the first number: \n");
+  scanf("%d", &int1);
+  printf("please enter the second number: \n");
+  scanf("%d", &int2);
+  printf("The first num is %d and the second is %d. \n", int1, int2);
+  return 0;
+}
